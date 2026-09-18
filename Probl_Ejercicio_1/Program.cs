397215874d72ca1b8aabb1e4fac6b31bb2a01d45
Anette Michelle de la Cruz Arzat <michellearzat807@gmail.com>
2026-09-18 07:17:45 -0600

    Agregar archivos de proyecto.

---
+++ Probl_Ejercicio_1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("==================================================================");
+Console.WriteLine("EJERCICIO 1: Cálculo de potencia de un motor CD (P = V * I)");
+Console.WriteLine("Clasificación:");
+Console.WriteLine(" - Potencia <= 120 W: CONSUMO NORMAL");
+Console.WriteLine(" - Potencia > 120 W: ADVERTENCIA: CONSUMO ELEVADO");
+Console.WriteLine("==================================================================\n");
+
+double voltaje, corriente, potencia;
+
+Console.Write("Ingrese el voltaje (V): ");
+voltaje = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese la corriente (I): ");
+corriente = Convert.ToDouble(Console.ReadLine());
+
+      
+potencia = voltaje * corriente;
+
+Console.WriteLine($"\nPotencia calculada: {potencia:F2} W");
+
+            if (potencia <= 120)
+            {
+                Console.WriteLine("Resultado: CONSUMO NORMAL");
+            }
+            else
+            {
+                Console.WriteLine("Resultado: ADVERTENCIA: CONSUMO ELEVADO");
+            }
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        
